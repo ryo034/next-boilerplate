bcba52c3018b663c4667a135ad3bc8cfd17bdd13
ryo034 <uni.nashi.034@gmail.com>
2020-11-08 21:15:48 +0900

    buildを修正

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
+  distDir: 'dist',
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname);
     return config;
