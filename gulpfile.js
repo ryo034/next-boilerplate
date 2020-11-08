066c7778de4ee7e4a45e120f507944f3dbe8dd05
ryo034 <uni.nashi.034@gmail.com>
2020-11-08 21:01:52 +0900

    eslintの修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
 const gulp = require('gulp');
 const awspublish = require('gulp-awspublish');
 const parallelize = require('concurrent-transform');
@@ -30,23 +29,31 @@ const config = {
 };
 
 // eslint-disable-next-line func-names
-task('deploy', function () {
+gulp.task('deploy', function () {
   // S3 オプションを使用して新しい publisher を作成する
   // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#constructor-property
-  const publisher = create(config);
-  let g = src(`./${config.distDir}/**`);
+  const publisher = awspublish.create(config);
+
+  let g = gulp.src(`./${config.distDir}/**`);
   // publisher は、上記で指定した Content-Length、Content-Type、および他のヘッダーを追加する
   // 指定しない場合、はデフォルトで x-amz-acl が public-read に設定される
   g = g.pipe(parallelize(publisher.publish(config.headers), config.concurrentUploads));
+
   // CDN のキャッシュを削除する
   if (config.distribution) {
+    console.log('Configured with CloudFront distribution');
     g = g.pipe(cloudfront(config));
+  } else {
+    console.log('No CloudFront distribution configured - skipping CDN invalidation');
   }
+
   // 削除したファイルを同期する
-  if (config.deleteOldVersions) g = g.pipe(publisher.sync());
+  if (config.deleteOldVersions) {
+    g = g.pipe(publisher.sync());
+  }
   // 連続したアップロードを高速化するためにキャッシュファイルを作成する
   g = g.pipe(publisher.cache());
   // アップロードの更新をコンソールに出力する
-  g = g.pipe(reporter());
+  g = g.pipe(awspublish.reporter());
   return g;
 });
